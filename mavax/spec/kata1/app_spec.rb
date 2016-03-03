1f22485bf83836c3904607b46604a2770246315b
MarValero <chicemav@gmail.com>
2016-03-03 18:15:51 +0000

    Refactor on spec

--- mavax/spec/kata1/app_spec.rb
+++ mavax/spec/kata1/app_spec.rb
@@ -3,14 +3,27 @@ require 'stringio'
 
 RSpec.describe App do
   describe '.run' do
-    it 'returns a report of lines of code per file' do
+
+    def get_output
       buffer = StringIO.new
       subject.run('spec/assets', buffer)
       buffer.rewind
-      lines = buffer.read.split("\n")
-      expect(lines[0]).to match(%r{\AAnalyzing.+spec/assets\Z})
+      buffer.read.split("\n")
+    end
+
+    it 'returns a report of lines of code per file' do
+      lines = get_output
       expect(lines[1]).to match(%r{spec/assets/file 11\Z})
       expect(lines[2]).to match(%r{spec/assets/folder/file 6\Z})
+    end
+
+    it 'returns the main directory' do
+      lines = get_output
+      expect(lines[0]).to match(%r{\AAnalyzing.+spec/assets\Z})
+    end
+
+    it 'returns the total number of lines' do
+      lines = get_output
       expect(lines[3]).to eq 'Total line count: 17'
     end
   end
